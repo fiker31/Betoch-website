2dabcc3cabd9cc31e680f4c6df484861eddb15c0
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-12 13:53:46 +0300

    Shared_Footer

---
+++ src/Shared/Footer.tsx
@@ -0,0 +1,58 @@
+import { FunctionComponent } from "react";
+import styles from "./RealEstateLandingPage.module.css";
+const RealEstateLandingPage: FunctionComponent = () => {
+    return (
+      <div className={styles.realEstateLandingPage}>
+        <div className={styles.frameParent}>
+        <div className={styles.logoGroup}>
+       
+       <div className={styles.logo1}>
+      <div className={styles.betoch}>
+        <img
+          className={styles.defaultABeautifulModernLogIcon}
+          alt=""
+          src="/default-a-beautiful-modern-logo-for-a-real-estate-website-na-2-e65a82fd1f624fc6b3dbd703aba7be35-1removebgpreview-1@2x.png"
+        />
+        <b className={styles.betoch1}>BETOCH</b>
+      </div>
+    </div>
+    
+   
+    <div className={styles.navigationBarWrapper}>
+      <div className={styles.navigationBar}>
+        <div className={styles.navbar1}>
+          <div className={styles.content}>
+            <a href="/real-estate-listing-page">
+              <div className={styles.search}>
+              <div className={styles.home}>Search</div>
+            </div>
+            
+            </a>
+            <a href="/">
+               <div className={styles.search}>
+              <div className={styles.home}>Home</div>
+            </div>
+            </a>
+           
+            <div className={styles.search}>
+              <div className={styles.home}>About</div>
+            </div>
+            <div className={styles.search}>
+              <div className={styles.home}>Help</div>
+            </div>
+            <div className={styles.blog}>
+              <div className={styles.blog1}>Blog</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className={styles.realEstateLandingPageItem} />
+  <div className={styles.copyright2023All}>
+    Copyright @ 2023. All Rights Reserved
+  </div>
+  <img className={styles.copyright1Icon} alt="" src="/copyright-1.svg" />
+</div>
+</div>
+            )}
