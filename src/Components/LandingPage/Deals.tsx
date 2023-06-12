ca5d54b1a23c9d3e38845f2fb0fae981ab0c28b5
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-12 11:55:07 +0300

    Deals_Page

---
+++ src/Components/LandingPage/Deals.tsx
@@ -0,0 +1,303 @@
+import { FunctionComponent } from "react";
+import styles from "./RealEstateLandingPage.module.css";
+
+
+const RealEstateLandingPage: FunctionComponent = () => {
+  return (
+    <div className={styles.frameParent}>
+    <div className={styles.realEstateLandingPageChild}>
+        <div className={styles.bestHouseDealParent}>
+          <b className={styles.bestHouseDealContainer}>
+            <span>{`Best House `}</span>
+            <span className={styles.works}>Deal</span>
+          </b>
+          <div className={styles.weHaveThe}>
+            We have the best deals to let the customers choose from it
+          </div>
+          <b className={styles.buy}>Buy</b>
+          <div className={styles.rent}>Rent</div>
+          <div className={styles.groupInner} />
+          <div className={styles.lineDiv} />
+          <a href="/real-estate-listing-page">
+             <div className={styles.rectangleDiv} />
+          <b className={styles.viewAll}>View all</b>
+          </a>
+         
+          <div className={styles.groupDiv}>
+            <div className={styles.groupChild1} />
+            <div className={styles.etb10000000}>{`ETB 10,000,000 `}</div>
+            <div className={styles.mexicoAddisAbaba}>
+              Mexico, Addis Ababa, Ethiopia
+            </div>
+            <div className={styles.groupContainer}>
+              <div className={styles.groupParent1}>
+                <div className={styles.forSaleWrapper}>
+                  <div className={styles.forSaleWrapper}>
+                    <div className={styles.forSaleWrapper}>
+                      <div className={styles.instanceChild} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.parent}>
+                  <div className={styles.div}>2</div>
+                  <div className={styles.angleSmallDown1} />
+                </div>
+                <img
+                  className={styles.bedAlt11Icon}
+                  alt=""
+                  src="/bedalt1-1.svg"
+                />
+              </div>
+              <div className={styles.groupParent2}>
+                <div className={styles.forSaleContainer}>
+                  <div className={styles.forSaleContainer}>
+                    <div className={styles.forSaleContainer}>
+                      <div className={styles.instanceChild} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.wrapper}>
+                  <div className={styles.div1}>1</div>
+                </div>
+                <img className={styles.bath1Icon} alt="" src="/bath-1.svg" />
+              </div>
+              <div className={styles.groupParent3}>
+                <div className={styles.forSaleFrame}>
+                  <div className={styles.forSaleFrame}>
+                    <div className={styles.forSaleFrame}>
+                      <div className={styles.instanceChild} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.m2Parent}>
+                  <div className={styles.m2}>60 m2</div>
+                  <div className={styles.angleSmallDown11} />
+                </div>
+                <img
+                  className={styles.rulerHorizontal1Icon}
+                  alt=""
+                  src="/rulerhorizontal-1.svg"
+                />
+              </div>
+            </div>
+          </div>
+          <img
+            className={styles.rectangleIcon}
+            alt=""
+            src="/rectangle-7@2x.png"
+          />
+          <img className={styles.vectorIcon} alt="" src="/vector4.svg" />
+          <div className={styles.rectangleParent1}>
+            <div className={styles.groupChild2} />
+            <div className={styles.forSale3}>For Sale</div>
+          </div>
+          <div className={styles.rectangleParent2}>
+            <div className={styles.groupChild1} />
+            <div className={styles.etb10000000}>{`ETB 10,000,000 `}</div>
+            <div className={styles.mexicoAddisAbaba}>
+              Mexico, Addis Ababa, Ethiopia
+            </div>
+            <div className={styles.groupContainer}>
+              <div className={styles.groupParent1}>
+                <div className={styles.forSaleWrapper}>
+                  <div className={styles.forSaleWrapper}>
+                    <div className={styles.forSaleWrapper}>
+                      <div className={styles.instanceChild} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.parent}>
+                  <div className={styles.div}>2</div>
+                  <div className={styles.angleSmallDown1} />
+                </div>
+                <img
+                  className={styles.bedAlt11Icon}
+                  alt=""
+                  src="/bedalt1-1.svg"
+                />
+              </div>
+              <div className={styles.groupParent2}>
+                <div className={styles.forSaleContainer}>
+                  <div className={styles.forSaleContainer}>
+                    <div className={styles.forSaleContainer}>
+                      <div className={styles.instanceChild} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.wrapper}>
+                  <div className={styles.div1}>1</div>
+                </div>
+                <img className={styles.bath1Icon} alt="" src="/bath-1.svg" />
+              </div>
+              <div className={styles.groupParent3}>
+                <div className={styles.forSaleFrame}>
+                  <div className={styles.forSaleFrame}>
+                    <div className={styles.forSaleFrame}>
+                      <div className={styles.instanceChild} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.m2Parent}>
+                  <div className={styles.m2}>60 m2</div>
+                  <div className={styles.angleSmallDown11} />
+                </div>
+                <img
+                  className={styles.rulerHorizontal1Icon}
+                  alt=""
+                  src="/rulerhorizontal-1.svg"
+                />
+              </div>
+            </div>
+          </div>
+          <img
+            className={styles.groupChild4}
+            alt=""
+            src="/rectangle-7@2x.png"
+          />
+          <img className={styles.vectorIcon1} alt="" src="/vector.svg" />
+          <div className={styles.rectangleParent3}>
+            <div className={styles.groupChild2} />
+            <div className={styles.forSale3}>For Sale</div>
+          </div>
+          <div className={styles.rectangleParent4}>
+            <div className={styles.groupChild1} />
+            <div className={styles.etb10000000}>{`ETB 10,000,000 `}</div>
+            <div className={styles.mexicoAddisAbaba}>
+              Mexico, Addis Ababa, Ethiopia
+            </div>
+            <div className={styles.groupContainer}>
+              <div className={styles.groupParent1}>
+                <div className={styles.forSaleWrapper}>
+                  <div className={styles.forSaleWrapper}>
+                    <div className={styles.forSaleWrapper}>
+                      <div className={styles.instanceChild} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.parent}>
+                  <div className={styles.div}>2</div>
+                  <div className={styles.angleSmallDown1} />
+                </div>
+                <img
+                  className={styles.bedAlt11Icon}
+                  alt=""
+                  src="/bedalt1-1.svg"
+                />
+              </div>
+              <div className={styles.groupParent2}>
+                <div className={styles.forSaleContainer}>
+                  <div className={styles.forSaleContainer}>
+                    <div className={styles.forSaleContainer}>
+                      <div className={styles.instanceChild} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.wrapper}>
+                  <div className={styles.div1}>1</div>
+                </div>
+                <img className={styles.bath1Icon} alt="" src="/bath-1.svg" />
+              </div>
+              <div className={styles.groupParent3}>
+                <div className={styles.forSaleFrame}>
+                  <div className={styles.forSaleFrame}>
+                    <div className={styles.forSaleFrame}>
+                      <div className={styles.instanceChild} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.m2Parent}>
+                  <div className={styles.m2}>60 m2</div>
+                  <div className={styles.angleSmallDown11} />
+                </div>
+                <img
+                  className={styles.rulerHorizontal1Icon}
+                  alt=""
+                  src="/rulerhorizontal-1.svg"
+                />
+              </div>
+            </div>
+          </div>
+          <img
+            className={styles.groupChild7}
+            alt=""
+            src="/rectangle-7@2x.png"
+          />
+          <img className={styles.vectorIcon2} alt="" src="/vector4.svg" />
+          <div className={styles.rectangleParent5}>
+            <div className={styles.groupChild2} />
+            <div className={styles.forSale3}>For Sale</div>
+          </div>
+          <div className={styles.rectangleParent6}>
+            <div className={styles.groupChild1} />
+            <div className={styles.etb10000000}>{`ETB 10,000,000 `}</div>
+            <div className={styles.mexicoAddisAbaba}>
+              Mexico, Addis Ababa, Ethiopia
+            </div>
+            <div className={styles.groupContainer}>
+              <div className={styles.groupParent1}>
+                <div className={styles.forSaleWrapper}>
+                  <div className={styles.forSaleWrapper}>
+                    <div className={styles.forSaleWrapper}>
+                      <div className={styles.instanceChild} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.parent}>
+                  <div className={styles.div}>2</div>
+                  <div className={styles.angleSmallDown1} />
+                </div>
+                <img
+                  className={styles.bedAlt11Icon}
+                  alt=""
+                  src="/bedalt1-1.svg"
+                />
+              </div>
+              <div className={styles.groupParent2}>
+                <div className={styles.forSaleContainer}>
+                  <div className={styles.forSaleContainer}>
+                    <div className={styles.forSaleContainer}>
+                      <div className={styles.instanceChild} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.wrapper}>
+                  <div className={styles.div1}>1</div>
+                </div>
+                <img className={styles.bath1Icon} alt="" src="/bath-1.svg" />
+              </div>
+              <div className={styles.groupParent3}>
+                <div className={styles.forSaleFrame}>
+                  <div className={styles.forSaleFrame}>
+                    <div className={styles.forSaleFrame}>
+                      <div className={styles.instanceChild} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.m2Parent}>
+                  <div className={styles.m2}>60 m2</div>
+                  <div className={styles.angleSmallDown11} />
+                </div>
+                <img
+                  className={styles.rulerHorizontal1Icon3}
+                  alt=""
+                  src="/rulerhorizontal-11.svg"
+                />
+              </div>
+            </div>
+          </div>
+          <img
+            className={styles.groupChild10}
+            alt=""
+            src="/rectangle-7@2x.png"
+          />
+          <div className={styles.rectangleParent7}>
+            <div className={styles.groupChild2} />
+            <div className={styles.forSale3}>For Sale</div>
+          </div>
+        </div>
+      </div>
+    </div>
+ 
+
+  )
+  }
