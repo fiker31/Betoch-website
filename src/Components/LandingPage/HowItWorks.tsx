a3b299f0649729d9fceea3c18dc52e53da10b53b
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-12 11:52:48 +0300

    How_It_Works

---
+++ src/Components/LandingPage/HowItWorks.tsx
@@ -0,0 +1,45 @@
+import { FunctionComponent } from "react";
+import styles from "./RealEstateLandingPage.module.css";
+
+
+const RealEstateLandingPage: FunctionComponent = () => {
+  return (
+    <div className={styles.frameParent}>
+    <div className={styles.realEstateLandingPageInner}>
+        <div className={styles.rectangleContainer}>
+          <div className={styles.groupItem} />
+          <div className={styles.instanceParent}>
+            <div className={styles.howItWorksParent}>
+              <b className={styles.howItWorksContainer}>
+                <span>{`How It `}</span>
+                <span className={styles.works}>Works</span>
+              </b>
+              <div className={styles.groupParent}>
+                <img className={styles.frameChild} alt="" src="/group-32.svg" />
+                <img className={styles.frameChild} alt="" src="/group-33.svg" />
+                <img className={styles.frameChild} alt="" src="/group-32.svg" />
+              </div>
+            </div>
+            <div className={styles.researchPlaces}>Research Places</div>
+            <div className={styles.weUseResearch}>
+              We use research places so that you get your desired place to live
+              a happy life.
+            </div>
+            <div className={styles.weUseInstant}>
+              We use instant valuation so that you get your desired place to
+              live a happy life.
+            </div>
+            <div className={styles.weUseTrack}>
+              We use track property so that you get your desired place to live a
+              happy life
+            </div>
+            <div className={styles.trackProperty}>Track Property</div>
+            <div className={styles.instantValuation}>Instant Valuation</div>
+          </div>
+        </div>
+    </div>
+    </div>
+ 
+
+  )
+  }
