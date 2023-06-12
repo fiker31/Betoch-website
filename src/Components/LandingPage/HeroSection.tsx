3da9f5b4c0fd94dc4b0087f3482cc3d19bdf98b5
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-12 11:48:36 +0300

    Hero_Section

--- src/Components/LandingPage/HeroSection.tsx
+++ src/Components/LandingPage/HeroSection.tsx
@@ -4,7 +4,44 @@ import styles from "./RealEstateLandingPage.module.css";
 
 const RealEstateLandingPage: FunctionComponent = () => {
   return (
-    <div className={styles.frameParent}></div>
-    
+    <div className={styles.frameParent}>
+    <div className={styles.groupChild} />
+    <div className={styles.houseToRentOrBuyParent}>
+    <b className={styles.houseToRent}>House To Rent or Buy</b>
+          <div className={styles.ifYoureLooking}>
+            If you’re looking for smart living then you’re at a right place.
+            Whether you’re buying or renting we’re here to help you out.
+          </div>
+          <div className={styles.findYourSmart}>
+            Find Your Smart Living Style
+          </div>
+          <b className={styles.k}>
+            <p className={styles.youllGetThe}>
+              <span>{`230 `}</span>
+              <span className={styles.k2}>K</span>
+            </p>
+          </b>
+          <div className={styles.houseSold}>
+            <p className={styles.youllGetThe}>House Sold</p>
+          </div>
+          <b className={styles.k3}>
+            <p className={styles.youllGetThe}>
+              <span>{`170 `}</span>
+              <span className={styles.k2}>K</span>
+            </p>
+          </b>
+          <div className={styles.houseAvailable}>
+            <p className={styles.youllGetThe}>House Available</p>
+          </div>
+        </div>
+        <a href="/real-estate-listing-page">
+          <div className={styles.rectangleGroup}>
+          <div className={styles.componentItem} />
+          <b className={styles.search2}>Search</b>
+        </div>
+        </a>
+    </div>
+ 
+
   )
   }
