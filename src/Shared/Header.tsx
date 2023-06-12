b73fd427422568aa5070082e168fee78789fbc4d
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-12 15:57:27 +0300

    footer_Page

--- src/Shared/Header.tsx
+++ src/Shared/Header.tsx
@@ -52,7 +52,7 @@ const RealEstateLandingPage: FunctionComponent = () => {
   <div className={styles.copyright2023All}>
     Copyright @ 2023. All Rights Reserved
   </div>
-  <img className={styles.copyright1Icon} alt="" src="/copyright-1.svg" />
+ 
 </div>
 </div>
             )}
