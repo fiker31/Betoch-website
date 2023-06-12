fb8237d76c87207faf56b0c97401d8f460446eef
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-12 09:30:54 +0300

    Hero_Section

---
+++ src/Components/HeroSection.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "react";
+import styles from "./RealEstateLandingPage.module.css";
+
+
+const RealEstateLandingPage: FunctionComponent = () => {
+  return (
+    <div className={styles.frameParent}></div>
+    
+  )
+  }
