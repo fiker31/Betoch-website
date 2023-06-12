5c558d569b16e42dc7da20b2c734e434632357ee
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-12 12:01:18 +0300

    Partners_Page

---
+++ src/Components/LandingPage/Partners.tsx
@@ -0,0 +1,36 @@
+import { FunctionComponent } from "react";
+import styles from "./RealEstateLandingPage.module.css";
+
+
+const RealEstateLandingPage: FunctionComponent = () => {
+  return (
+    <div className={styles.frameParent}>
+         <div className={styles.realEstateLandingPageInner2}>
+        <div className={styles.rectangleParent8}>
+          <div className={styles.groupChild26} />
+          <b className={styles.ourPartners}>
+            <span>{`Our `}</span>
+            <span className={styles.works}>Partners</span>
+          </b>
+          <img
+            className={styles.christmas2012New4191RemoveIcon}
+            alt=""
+            src="/christmas-2012-new-4191removebgpreview-1@2x.png"
+          />
+          <img
+            className={styles.removebgPreview1Icon}
+            alt=""
+            src="/2removebgpreview-1@2x.png"
+          />
+          <img
+            className={styles.removebgPreview1Icon1}
+            alt=""
+            src="/3removebgpreview-1@2x.png"
+          />
+        </div>
+        </div>
+    </div>
+ 
+
+  )
+  }
