6b156bcb781e461aea27b1005f27a11a04c9c022
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-12 13:52:34 +0300

    Shared Footer

--- src/Shared/Header.tsx
+++ src/Shared/Header.tsx
@@ -4,56 +4,55 @@ const RealEstateLandingPage: FunctionComponent = () => {
     return (
       <div className={styles.realEstateLandingPage}>
         <div className={styles.frameParent}>
-        <div className={styles.groupWrapper}>
-<div className={styles.logoParent}>
-  <div className={styles.logo}>
-    <div className={styles.betoch}>
-      <img
-        className={styles.defaultABeautifulModernLogIcon}
-        alt=""
-        src="/default-a-beautiful-modern-logo-for-a-real-estate-website-na-2-e65a82fd1f624fc6b3dbd703aba7be35-1removebgpreview-1@2x.png"
-      />
-      <b className={styles.betoch1}>BETOCH</b>
+        <div className={styles.logoGroup}>
+       
+       <div className={styles.logo1}>
+      <div className={styles.betoch}>
+        <img
+          className={styles.defaultABeautifulModernLogIcon}
+          alt=""
+          src="/default-a-beautiful-modern-logo-for-a-real-estate-website-na-2-e65a82fd1f624fc6b3dbd703aba7be35-1removebgpreview-1@2x.png"
+        />
+        <b className={styles.betoch1}>BETOCH</b>
+      </div>
     </div>
-  </div>
-  <div className={styles.icon}>
-    <img className={styles.heartIcon} alt="" src="/heart.svg" />
-  </div>
-  <div className={styles.navigationBarParent}>
-    <div className={styles.navigationBar}>
-      <div className={styles.navbar}>
-        <div className={styles.content}>
-          <a href="/real-estate-listing-page">
-          <div className={styles.search}> 
-        <div className={styles.home}>Search</div>
-         </div>
-          </a>
-       <a href="/">
-       <div className={styles.about}>
-           <b className={styles.home}>Home</b>
-          </div>
-       </a>
-         
-
-          <div className={styles.search}>
-            <div className={styles.home}>About</div>
-          </div>
-          <div className={styles.search}>
-            <div className={styles.home}>Help</div>
-          </div>
-          <div className={styles.blog}>
-            <div className={styles.blog1}>Blog</div>
+    
+   
+    <div className={styles.navigationBarWrapper}>
+      <div className={styles.navigationBar}>
+        <div className={styles.navbar1}>
+          <div className={styles.content}>
+            <a href="/real-estate-listing-page">
+              <div className={styles.search}>
+              <div className={styles.home}>Search</div>
+            </div>
+            
+            </a>
+            <a href="/">
+               <div className={styles.search}>
+              <div className={styles.home}>Home</div>
+            </div>
+            </a>
+           
+            <div className={styles.search}>
+              <div className={styles.home}>About</div>
+            </div>
+            <div className={styles.search}>
+              <div className={styles.home}>Help</div>
+            </div>
+            <div className={styles.blog}>
+              <div className={styles.blog1}>Blog</div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div className={styles.groupChild} />
   </div>
-  <div className={styles.man1Wrapper}>
-    <img className={styles.man1Icon} alt="" src="/man-1@2x.png" />
+  <div className={styles.realEstateLandingPageItem} />
+  <div className={styles.copyright2023All}>
+    Copyright @ 2023. All Rights Reserved
   </div>
-</div>
-</div>
+  <img className={styles.copyright1Icon} alt="" src="/copyright-1.svg" />
 </div>
 </div>
             )}
