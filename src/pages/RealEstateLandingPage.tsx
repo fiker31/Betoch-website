1efb537db03bec549396cf6e233d4a4c72107a90
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-13 11:54:01 +0300

    Landing Page components

--- src/pages/RealEstateLandingPage.tsx
+++ src/pages/RealEstateLandingPage.tsx
@@ -20,7 +20,6 @@ const RealEstateLandingPage: FunctionComponent = () => {
             <Demanding />            
             <HowItWorks />
             <Deals />
-            
             <Testimonials />
             <Partners />
             <Footer />
